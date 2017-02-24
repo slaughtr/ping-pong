0a4ecceba2ca49128e451e7cc47e31370e5d90f7
slaughtr <dallas.xiao@gmail.com>
2017-02-24 10:02:24 -0800

    Add css and js for animations on display

--- js/scripts.js
+++ js/scripts.js
@@ -13,35 +13,33 @@ function convertToPingPong() { //convert array items to ping/pong/pingpong
   var index = 0;
   countingArray.forEach(function(number) {
     if (number % 15 === 0) {
-      countingArray[index] = "\u221EPINGPONG\u221E";
+      countingArray[index] = "class='pingpong'>\u221EPINGPONG\u221E";
     } else if (number % 3 === 0) {
-      countingArray[index] = "\u2022PING\u2022";
+      countingArray[index] = "class='ping'>\u2022PING\u2022";
     } else if (number % 5 === 0) {
-      countingArray[index] = "\u00B0PONG\u00B0";
+      countingArray[index] = "class='pong'>\u00B0PONG\u00B0";
+    } else {
+      countingArray[index] = "class='number'>"+number
     }
     index++;
-  });
-  convertArrayToList();
-}
-
-function convertArrayToList(input) { //this is the bit that outputs the items onto the page
-  countingArray.forEach(function(output){
-    $("#resultDisplay").append("<li class='output'>" + output + "</li>");
   });
 }
 
 //user interface logic
 $(document).ready(function(){
-
   $("form#resetPage").submit(function(){
     //I can't get it to reset my list any other way. all .remove, .empty, etc functions are non-operational. So, let's not prevent that default, I guess.
   });
-
   $("form#userInput").submit(function(){
     event.preventDefault();
     var value = $("#inputString").val();
-
-
     inputToArray(value);
+    countingArray.forEach(function(output){
+      $("#resultDisplay").append("<li " + output + "</li>");
+    });
+    $(".ping").slideDown(1000);
+    $(".pong").show(2222);
+    $(".pingpong").fadeIn(1212);
+    $(".number").show(4444);
   });
 });
